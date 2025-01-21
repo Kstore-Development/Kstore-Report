99f785ab2ed9691acdcb57be3177144ad548b230
K1anFromDK <108903326+K1anFromDK@users.noreply.github.com>
2025-01-21 16:09:29 +0100

    Update config.lua

--- kst-report/shared/config.lua
+++ kst-report/shared/config.lua
@@ -1,5 +1,5 @@
 Config = {}
 
-Config.Webhook = 'https://discord.com/api/webhooks/1276706092176375869/rrPeFhvXvxOdOz_sr3NuXkue5jnk7FD4VS3DdYV8VcYmiv2RxCGnBZOWIIHkfLgcH00W'
+Config.Webhook = 'WEBHOOK'
 
 Config.Framework = 'STANDALONE' -- VRP - STANDALONE
